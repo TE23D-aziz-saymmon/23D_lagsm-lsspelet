810d21dc8a85d4c88e6e818d11b55800bf189b65
Saymmon Aziz <saymmon.aziz@elev.ga.ntig.se>
2024-09-20 10:10:10 +0200

    Relax fam

---
+++ Slagsmålsspelet/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+Console.WriteLine("Skriv in ditt namn:");
+
+string name = Console.ReadLine();
+while (name.Length < 3)
+{
+    Console.WriteLine("Skriv in ditt namn, sa jag, dumhuvve!");
+    name = Console.ReadLine();
+}
+
+
+Console.WriteLine($"Hej {name}");
+
+Console.ReadLine();
