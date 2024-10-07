9cfebc576d6043acc8b6549a406ad1829cd70dc3
Saymmon Aziz <saymmon.aziz@elev.ga.ntig.se>
2024-10-07 08:57:10 +0200

    ss

--- Slagsmålsspelet/obj/Debug/net8.0/Slagsmålsspelet.AssemblyInfo.cs
+++ Slagsmålsspelet/obj/Debug/net8.0/Slagsmålsspelet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Slagsmålsspelet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bb1d70941fc9344b5b5d472b4cd8c5f0d984a90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db9ef0eb114b96a5dc25bd07310305155f464cf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Slagsmålsspelet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Slagsmålsspelet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
